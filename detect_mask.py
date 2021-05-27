8189b10edcc68d7475f15c3f78d9340d489746e0
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2021-05-27 10:38:08 +0530

    Add get_name function to perform face_recognition

--- detect_mask.py
+++ detect_mask.py
@@ -16,6 +16,46 @@ import clx.xms
 import requests
 
 
+def get_name(frame):
+        faces_encodings = []
+        faces_names = []
+        _path = os.path.join('dataset/' + 'images/')
+
+        list_of_files = [i for i in glob.glob(_path+'*.jpg')]
+
+        number_files = len(list_of_files)
+        names = list_of_files.copy()                
+
+
+        for i in range(number_files):
+            globals()['image_{}'.format(i)] = face_recognition.load_image_file(list_of_files[i])
+            globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
+            faces_encodings.append(globals()['image_encoding_{}'.format(i)])
+
+            names[i] = names[i].replace(_path, "")  
+            faces_names.append(names[i]) 
+
+        face_locations = []
+        face_encodings = []
+        face_names = []
+        process_this_frame = True
+
+        
+        rgb_small_frame = frame[:, :, ::-1]
+        if process_this_frame:
+            face_locations = face_recognition.face_locations( rgb_small_frame)
+            face_encodings = face_recognition.face_encodings( rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces (faces_encodings, face_encoding)
+                name = "Unknown"
+                face_distances = face_recognition.face_distance( faces_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = faces_names[best_match_index]
+                face_names.append(name.split(".")[0])
+
+        return face_names 
 
 
 
